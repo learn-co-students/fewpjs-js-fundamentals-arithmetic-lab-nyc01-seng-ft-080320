9d43fde5ab2d844aa66b8a22c61030dd4c544184
royFloresNyc <royfloresnyc@gmail.com>
2020-09-13 19:45:52 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 5 ) + 1;
+
+let mod = 28 % 6;
+
+let max = Math.max(1, 20, 16, 7, 13);
