11d08d8ddcef37e862abf6b33df461f6febc5655
anickel101 <anson.nickel@gmail.com>
2020-09-13 12:24:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// Assignment #1
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+// Assingment #2
+let random = Math.floor(Math.random() * 100 +1)
+
+// Assignment #3
+let num3 = 19
+let num4 = 5
+let mod = num3 % num4 // should equal 4, 5*3 + 4 = 19
+
+// Assignmnet #4
+let nums = [...Array(21).keys()]
+let max = Math.max(...nums)
