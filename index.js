f9f24521fe70f68d9764d387b730006f44d216f7
alerner1 <alyssplerner@gmail.com>
2020-09-14 18:51:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+const mod = 10 % 6;
+
+const random = Math.floor(Math.random() * 1000);
+
+const max = Math.max(18, 20);
+
